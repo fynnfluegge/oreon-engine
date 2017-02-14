7123503e942a795770acb05a89a09e8fee8d6329
Fynn <fynnfluegge@gmx.de>
2017-02-14 16:19:57 +0100

    02142017

---
+++ oreon.engine/res/oreonworlds/shaders/Grass_01/Grass01_FS.glsl
@@ -0,0 +1,122 @@
+#version 330
+
+in vec3 position_FS;
+in vec2 texCoord_FS;
+in vec4 viewSpacePos;
+
+struct Material
+{
+	sampler2D diffusemap;
+};
+
+layout (std140, row_major) uniform Camera{
+	vec3 eyePosition;
+	mat4 m_View;
+	mat4 viewProjectionMatrix;
+	vec4 frustumPlanes[6];
+};
+
+layout (std140) uniform DirectionalLight{
+	vec3 direction;
+	float intensity;
+	vec3 ambient;
+	vec3 color;
+} directional_light;
+
+layout (std140, row_major) uniform LightViewProjections{
+	mat4 m_lightViewProjection[6];
+	float splitRange[6];
+};
+	
+uniform sampler2DArray shadowMaps;
+uniform Material material;
+uniform float sightRangeFactor;
+
+const float zFar = 10000;
+const vec3 fogColor = vec3(0.62,0.85,0.95);
+
+
+float diffuse(vec3 direction, vec3 normal, float intensity)
+{
+	return max(0.0, dot(normal, -direction) * intensity);
+}
+
+float varianceShadow(vec3 projCoords, int split){
+	
+	float shadowFactor = 1.0;
+	float texelSize = 1.0/ 2048.0;
+	float currentDepth = projCoords.z;
+	
+	for (int i=-1; i<=1; i++){
+		for (int j=-1; j<=1; j++){
+			float shadowMapDepth = texture(shadowMaps, vec3(projCoords.xy,split)
+													   + vec3(i,j,0) * texelSize).r; 
+			if (currentDepth - 0.002 > shadowMapDepth)
+				shadowFactor -= 0.1;
+		}
+	}
+	
+	return shadowFactor;
+}
+
+float shadow(vec3 worldPos)
+{
+	float shadow = 1;
+	float shadowFactor = 0;
+	vec3 projCoords = vec3(0,0,0);
+	float depth = viewSpacePos.z/zFar;
+	if (depth < splitRange[0]){
+		vec4 lightSpacePos = m_lightViewProjection[0] * vec4(worldPos,1.0);
+		projCoords = lightSpacePos.xyz * 0.5 + 0.5;
+		shadowFactor = varianceShadow(projCoords,0);
+	}
+	else if (depth < splitRange[1]){
+		vec4 lightSpacePos = m_lightViewProjection[1] * vec4(worldPos,1.0);
+		projCoords = lightSpacePos.xyz * 0.5 + 0.5;
+		shadowFactor = varianceShadow(projCoords,1);
+	}
+	else if (depth < splitRange[2]){
+		vec4 lightSpacePos = m_lightViewProjection[2] * vec4(worldPos,1.0);
+		projCoords = lightSpacePos.xyz * 0.5 + 0.5;
+		shadowFactor = varianceShadow(projCoords,2);
+	}
+	else if (depth < splitRange[3]){
+		vec4 lightSpacePos = m_lightViewProjection[3] * vec4(worldPos,1.0);
+		projCoords = lightSpacePos.xyz * 0.5 + 0.5;
+		shadowFactor = varianceShadow(projCoords,3);
+	}
+	else if (depth < splitRange[4]){
+		vec4 lightSpacePos = m_lightViewProjection[4] * vec4(worldPos,1.0);
+		projCoords = lightSpacePos.xyz * 0.5 + 0.5;
+		shadowFactor = varianceShadow(projCoords,4); 
+	}
+	else if (depth < splitRange[5]){
+		vec4 lightSpacePos = m_lightViewProjection[5] * vec4(worldPos,1.0);
+		projCoords = lightSpacePos.xyz * 0.5 + 0.5;
+		shadowFactor = varianceShadow(projCoords,5); 
+	}
+	else return 1;
+
+	return shadowFactor;
+}
+
+void main()
+{	
+	float dist = length(eyePosition - position_FS);
+
+	float diffuseFactor = diffuse(directional_light.direction, vec3(0,1,0), directional_light.intensity);
+	float specularFactor = 0;
+	
+	
+	vec3 diffuseLight = directional_light.color * diffuseFactor;
+	
+	vec3 fragColor = texture(material.diffusemap, texCoord_FS).rgb;// * (directional_light.ambient + diffuseLight);
+	//fragColor *= shadow(position_FS);
+	float alpha = texture(material.diffusemap, texCoord_FS).a;
+	
+	float fogFactor = -0.0005/sightRangeFactor*(dist-zFar/5*sightRangeFactor);
+	
+    vec3 rgb = mix(fogColor, fragColor, clamp(fogFactor,0,1));
+	
+	gl_FragColor = vec4(rgb,1);
+}
---
+++ oreon.engine/res/oreonworlds/shaders/Grass_01/Grass01_GS.glsl
@@ -0,0 +1,47 @@
+#version 430
+
+layout(triangles, invocations = 1) in;
+
+layout(triangle_strip, max_vertices = 3) out;
+
+in int instanceID_GS[];
+in vec2 texCoord_GS[];
+
+out vec3 position_FS;
+out vec2 texCoord_FS;
+out vec4 viewSpacePos;
+
+layout (std140, row_major) uniform InstancedMatrices{
+	mat4 m_World[50];
+	mat4 m_Model[50];
+};
+
+layout (std140, row_major) uniform Camera{
+	vec3 eyePosition;
+	mat4 m_View;
+	mat4 viewProjectionMatrix;
+	vec4 frustumPlanes[6];
+};
+
+uniform vec4 clipplane;
+
+void main()
+{	
+	for (int i = 0; i < gl_in.length(); ++i)
+	{
+		vec4 worldPos = m_World[ instanceID_GS[i] ] * gl_in[i].gl_Position;
+		gl_Position = viewProjectionMatrix * worldPos;
+		gl_ClipDistance[0] = dot(gl_Position,frustumPlanes[0]);
+		gl_ClipDistance[1] = dot(gl_Position,frustumPlanes[1]);
+		gl_ClipDistance[2] = dot(gl_Position,frustumPlanes[2]);
+		gl_ClipDistance[3] = dot(gl_Position,frustumPlanes[3]);
+		gl_ClipDistance[4] = dot(gl_Position,frustumPlanes[4]);
+		gl_ClipDistance[5] = dot(gl_Position,frustumPlanes[5]);
+		gl_ClipDistance[6] = dot(gl_Position,clipplane);
+		texCoord_FS = texCoord_GS[i];
+		position_FS = worldPos.xyz;
+		viewSpacePos = m_View * worldPos;
+		EmitVertex();
+	}	
+	EndPrimitive();
+}
