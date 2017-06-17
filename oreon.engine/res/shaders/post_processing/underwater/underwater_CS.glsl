ada112b66a0c8a2c37f5d0af9f763503b2226763
Fynn <fynnfluegge@gmx.de>
2017-06-17 22:09:01 +0200

    v2.1 Beta; Caustics Feature added

---
+++ oreon.engine/res/shaders/post_processing/underwater/underwater_CS.glsl
@@ -0,0 +1,42 @@
+#version 430 core
+
+layout (local_size_x = 8, local_size_y = 8) in;
+
+layout (binding = 0, rgba16f) uniform readonly image2D sceneSampler;
+
+layout (binding = 1, rgba16f) uniform writeonly image2D underwaterSceneSampler;
+
+uniform sampler2D sceneDepthMap;
+uniform float windowWidth;
+uniform float windowHeight;
+
+const float zfar = 10000.0f;
+const float znear = 0.1f;
+
+const vec3 waterRefractionColor = vec3(0.015,0.022,0.06);
+
+float linearize(float depth)
+{
+	return (2 * znear) / (zfar + znear - depth * (zfar - znear));
+	// return (2*depth - zfar - znear) / (zfar - znear) - 1;
+}
+
+void main(){
+
+	ivec2 computeCoord = ivec2(gl_GlobalInvocationID.x, gl_GlobalInvocationID.y);
+	
+	// window coords
+	vec2 w = vec2(gl_GlobalInvocationID.x/windowWidth, gl_GlobalInvocationID.y/windowHeight);
+	
+	// Get the depth buffer value at this pixel.  
+	float depth = linearize(texture(sceneDepthMap, w).r); 
+	
+	vec3 sceneColor = imageLoad(sceneSampler, computeCoord).rgb;
+	
+	float refractionFactor = clamp(2 * pow(depth,0.2) + 0.2,0.0,0.96);
+	
+	vec3 rgb = mix(sceneColor, waterRefractionColor, refractionFactor); 
+	
+	
+	imageStore(underwaterSceneSampler, computeCoord, vec4(rgb,1));
+}
