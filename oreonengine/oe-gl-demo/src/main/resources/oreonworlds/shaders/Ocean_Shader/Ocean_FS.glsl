c270d54277735da32a26b51ddea7babb2ec33005
Fynn <fynnfluegge@gmx.de>
2017-11-28 22:07:17 +0100

    v3.0.0 BETA build 11282017

--- oreonengine/oe-gl-demo/src/main/resources/oreonworlds/shaders/Ocean_Shader/Ocean_FS.glsl
+++ oreonengine/oe-gl-demo/src/main/resources/oreonworlds/shaders/Ocean_Shader/Ocean_FS.glsl
@@ -95,7 +95,7 @@ void main(void)
     // Reflection //
 	vec2 reflecCoords = projCoord.xy + dudvCoord.rb * kReflection;
 	reflecCoords = clamp(reflecCoords, kReflection, 1-kReflection);
-    vec3 reflection = mix(texture(waterReflection, reflecCoords).rgb, deepOceanColor,  0.4);
+    vec3 reflection = mix(texture(waterReflection, reflecCoords).rgb, deepOceanColor,  0.5);
     reflection *= F;
  
     // Refraction //
